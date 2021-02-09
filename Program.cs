a451cf63ad1554ce4ffdde5403edd4cc6450e487
Vanesa <57409926+vanesa33@users.noreply.github.com>
2021-02-09 14:25:28 -0300

    Operadores relacionales
    Proyecto para principiante en programación c sharp, practicas

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operadores_relacionales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //operadores logicos
+
+            double peso; byte edad;
+
+            Console.WriteLine("digite su peso");
+            peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("digite tu edad");
+            edad = Convert.ToByte(Console.ReadLine());
+
+            Console.Clear();
+
+            if (peso > 100 && edad >= 15)
+            {
+                Console.WriteLine("tu peso es normal");
+
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
